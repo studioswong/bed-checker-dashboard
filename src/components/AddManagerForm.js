98e136bb25409514605af238585287224ee5003e
christian <christian.froh@googlemail.com>
2020-04-01 13:49:06 +0200

    apply design for dashboard and forms

--- src/components/AddManagerForm.js
+++ src/components/AddManagerForm.js
@@ -1,239 +1,267 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
 import {
-    Button,
-    Card,
-    CardHeader,
-    CardBody,
-    FormGroup,
-    Form,
-    Input,
-    Row,
-    Col,
-    DropdownToggle,
-    InputGroupButtonDropdown,
-    DropdownMenu,
-    DropdownItem
-  } from "reactstrap";
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  FormGroup,
+  Form,
+  Input,
+  Row,
+  Col,
+  DropdownToggle,
+  InputGroupButtonDropdown,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
-import gql from 'graphql-tag';
-import { useMutation } from '@apollo/react-hooks';
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/react-hooks";
 
 const CREATE_HOSPITAL_MANAGER_MUTATION = gql`
-mutation CreateHospitalManager(
-        $email: String!, 
-        $firstname: String!,
-        $lastname: String!,
-        $hospitalId: ID!,
-        $password: String!,
-        $phoneNumber: String ) {
-    createHospitalManager(input: {email: $email, firstname: $firstname, lastname: $lastname, hospitalId: $hospitalId, password: $password, phoneNumber: $phoneNumber}) {
-        hospitalManager {
-            id
-        }
+  mutation CreateHospitalManager(
+    $email: String!
+    $firstname: String!
+    $lastname: String!
+    $hospitalId: ID!
+    $password: String!
+    $phoneNumber: String
+  ) {
+    createHospitalManager(
+      input: {
+        email: $email
+        firstname: $firstname
+        lastname: $lastname
+        hospitalId: $hospitalId
+        password: $password
+        phoneNumber: $phoneNumber
+      }
+    ) {
+      hospitalManager {
+        id
+      }
     }
-}
+  }
 `;
 
-const AddManagerForm = (props) => {
-    const [ selectedHospital, setSelectedHospital ] = React.useState()
-    const [ hospitalId, setHospitalId ] = React.useState()
-    const [ firstname, setFirstname ] = React.useState()
-    const [ lastname, setLastname ] = React.useState()
-    const [ email, setEmail ] = React.useState()
-    const [ phoneNumber, setPhoneNumber ] = React.useState()
-    const [ password, setPassword ] = React.useState()
+const AddManagerForm = props => {
+  const [selectedHospital, setSelectedHospital] = React.useState();
+  const [hospitalId, setHospitalId] = React.useState();
+  const [firstname, setFirstname] = React.useState();
+  const [lastname, setLastname] = React.useState();
+  const [email, setEmail] = React.useState();
+  const [phoneNumber, setPhoneNumber] = React.useState();
+  const [password, setPassword] = React.useState();
 
-    const [ hasError, setHasError ] = React.useState(false)
-    const [ submitSuccess, setSubmitSuccess ] = React.useState(false)
+  const [hasError, setHasError] = React.useState(false);
+  const [submitSuccess, setSubmitSuccess] = React.useState(false);
 
-    const [dropdownOpen, setDropdownOpen] = useState(false);
-    const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
-    const handleHospital = (e) => {
-        const hospitalId = props.hospitals.filter(hospital => hospital.name == e.target.innerText)[0].id
-        setHospitalId(hospitalId)
-        setSelectedHospital(e.target.innerText)
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggleDropDown = () => setDropdownOpen(!dropdownOpen);
+  const handleHospital = e => {
+    const hospitalId = props.hospitals.filter(
+      hospital => hospital.name == e.target.innerText
+    )[0].id;
+    setHospitalId(hospitalId);
+    setSelectedHospital(e.target.innerText);
+  };
+  const [addHospitalManager, { data, error, loading }] = useMutation(
+    CREATE_HOSPITAL_MANAGER_MUTATION
+  );
+  const submitForm = e => {
+    e.preventDefault();
+    console.log("**submitted");
+    addHospitalManager({
+      variables: {
+        firstname,
+        lastname,
+        email,
+        phoneNumber,
+        password,
+        hospitalId
+      }
+    });
+    // useEffect(() => {
+    //     addHospitalManager({ variables: { firstname, lastname, email, phoneNumber, password, hospitalId } });
+    // }, []);
+  };
+
+  useEffect(() => {
+    if (error) setHasError(true);
+    if (data) setSubmitSuccess(true);
+  }, []);
+
+  // if (error) {
+  //     useEffect(() => {
+  //         setHasError(true)
+  //     }, []);
+  // }
+
+  // if (data) {
+  //     useEffect(() => {
+  //         setHasError(true)
+  //     }, []);
+  //     setSubmitSuccess(true)
+  // }
+
+  useEffect(() => {
+    if (data) {
+      setSubmitSuccess(true);
     }
-    const [ addHospitalManager, { data, error, loading }] = useMutation(CREATE_HOSPITAL_MANAGER_MUTATION);
-    const submitForm = e => {
-        e.preventDefault();
-        console.log('**submitted')
-        addHospitalManager({ variables: { firstname, lastname, email, phoneNumber, password, hospitalId } });
-        // useEffect(() => {
-        //     addHospitalManager({ variables: { firstname, lastname, email, phoneNumber, password, hospitalId } });
-        // }, []);
-    }
+  }, [data]);
 
-    useEffect(() => {
-        if(error) setHasError(true)
-        if(data) setSubmitSuccess(true)
-    }, []);
-
-    // if (error) {
-    //     useEffect(() => {
-    //         setHasError(true)
-    //     }, []);
-    // }
-    
-    // if (data) {
-    //     useEffect(() => {
-    //         setHasError(true)
-    //     }, []);
-    //     setSubmitSuccess(true)
-    // }
-
-    useEffect(() => {
-    if(data) {setSubmitSuccess(true)};
-    }, [ data ])
-
-    return(
-        <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
-        <Card className="bg-secondary shadow">
-          <CardHeader className="bg-white border-0">
-            <Row className="align-items-center">
-              <Col xs="8">
-                <h3 className="mb-0">Add COVID ward manager</h3>
-              </Col>
-            </Row>
-          </CardHeader>
-          <CardBody>
-              <Form>
-                  <h6 className="heading-small text-muted mb-4">
-                  Manager Contact information
-                  </h6>
-                  <div className="pl-lg-4">
-                      <Row>
-                          <Col md="12">
-                              <FormGroup>
-                                  <label
-                                  className="form-control-label"
-                                  htmlFor="input-address"
-                                  >
-                                  First name
-                                  </label>
-                                  <Input
-                                      className="form-control-alternative"
-                                      id="manager-input-first-name"
-                                      placeholder="John"
-                                      type="text"
-                                      onChange={(e) => setFirstname(e.target.value)}
-                                  />
-                              </FormGroup>
-                          </Col>
-                          <Col md="12">
-                              <FormGroup>
-                                  <label
-                                  className="form-control-label"
-                                  htmlFor="input-address"
-                                  >
-                                  Last name
-                                  </label>
-                                  <Input
-                                      className="form-control-alternative"
-                                      id="manager-input-last-name"
-                                      placeholder="Doe"
-                                      type="text"
-                                      onChange={(e) => setLastname(e.target.value)}
-                                  />
-                              </FormGroup>
-                          </Col>
-                          <Col md="12">
-                              <FormGroup>
-                                  <label
-                                  className="form-control-label"
-                                  htmlFor="input-address"
-                                  >
-                                  Email address
-                                  </label>
-                                  <Input
-                                      className="form-control-alternative"
-                                      id="manager-input-email"
-                                      placeholder="johndoe@hospital.com"
-                                      type="text"
-                                      onChange={(e) => setEmail(e.target.value)}
-                                  />
-                              </FormGroup>
-                          </Col>
-                          <Col md="12">
-                              <FormGroup>
-                                  <label
-                                  className="form-control-label"
-                                  htmlFor="input-address"
-                                  >
-                                  Phone number
-                                  </label>
-                                  <Input
-                                      className="form-control-alternative"
-                                      id="manager-input-phone-number"
-                                      placeholder="07998889078"
-                                      type="text"
-                                      onChange={(e) => setPhoneNumber(e.target.value)}
-                                  />
-                              </FormGroup>
-                          </Col>
-                          <Col md="12">
-                              <FormGroup>
-                                  <label
-                                  className="form-control-label"
-                                  htmlFor="input-address"
-                                  >
-                                  Password
-                                  </label>
-                                  <Input
-                                      className="form-control-alternative"
-                                      id="manager-input-passwordr"
-                                      placeholder="123123123"
-                                      type="text"
-                                      onChange={(e) => setPassword(e.target.value)}
-                                  />
-                              </FormGroup>
-                          </Col>
-                          <Col md="12">
-                              <FormGroup>
-                                  <label
-                                  className="form-control-label"
-                                  htmlFor="input-address"
-                                  >
-                                  Hospital Ward
-                                  </label>
-                                  <InputGroupButtonDropdown 
-                                    addonType="append" 
-                                    isOpen={dropdownOpen} 
-                                    toggle={toggleDropDown} 
-                                    className="form-control-alternative">
-                                      <DropdownToggle caret>
-                                        {selectedHospital || 'Choose Hospital'}
-                                      </DropdownToggle>
-                                      <DropdownMenu>
-                                          {props.hospitals.map(hospital => {
-                                              return (
-                                                <div>
-                                                    <DropdownItem onClick={(e) => handleHospital(e)}>{hospital.name}</DropdownItem>
-                                                    <DropdownItem divider />
-                                                </div>
-                                              )
-                                          })}
-                                      </DropdownMenu>
-                                  </InputGroupButtonDropdown>
-                              </FormGroup>
-                          </Col>
-                      </Row>
-                  </div>
-                    {hasError && <p>There is an error with adding the manager</p>}
-                    {submitSuccess && <p>The form has been submitted</p>}
-                    <Button
-                        color="info"
-                        href="#pablo"
-                        type="submit"
-                        onClick={(e) => submitForm(e)}
-                        >
-                        Add manager
-                    </Button>
-              </Form>
-          </CardBody>
-        </Card>
-      </Col>
-    )
-}
+  return (
+    <Col className="order-xl-2 mb-5 mb-xl-0" xl="4">
+      <Card className="bg-secondary shadow">
+        <CardHeader className="bg-white border-0">
+          <Row className="align-items-center">
+            <Col xs="8">
+              <h3 className="mb-0">Add COVID ward manager</h3>
+            </Col>
+          </Row>
+        </CardHeader>
+        <CardBody>
+          <Form>
+            <h6 className="heading-small text-muted mb-4">
+              Manager Contact information
+            </h6>
+            <div className="pl-lg-4">
+              <Row>
+                <Col md="12">
+                  <FormGroup>
+                    <label
+                      className="form-control-label"
+                      htmlFor="input-address"
+                    >
+                      First name
+                    </label>
+                    <Input
+                      className="form-control-alternative"
+                      id="manager-input-first-name"
+                      placeholder="John"
+                      type="text"
+                      onChange={e => setFirstname(e.target.value)}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md="12">
+                  <FormGroup>
+                    <label
+                      className="form-control-label"
+                      htmlFor="input-address"
+                    >
+                      Last name
+                    </label>
+                    <Input
+                      className="form-control-alternative"
+                      id="manager-input-last-name"
+                      placeholder="Doe"
+                      type="text"
+                      onChange={e => setLastname(e.target.value)}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md="12">
+                  <FormGroup>
+                    <label
+                      className="form-control-label"
+                      htmlFor="input-address"
+                    >
+                      Email address
+                    </label>
+                    <Input
+                      className="form-control-alternative"
+                      id="manager-input-email"
+                      placeholder="johndoe@hospital.com"
+                      type="text"
+                      onChange={e => setEmail(e.target.value)}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md="12">
+                  <FormGroup>
+                    <label
+                      className="form-control-label"
+                      htmlFor="input-address"
+                    >
+                      Phone number
+                    </label>
+                    <Input
+                      className="form-control-alternative"
+                      id="manager-input-phone-number"
+                      placeholder="07998889078"
+                      type="text"
+                      onChange={e => setPhoneNumber(e.target.value)}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md="12">
+                  <FormGroup>
+                    <label
+                      className="form-control-label"
+                      htmlFor="input-address"
+                    >
+                      Password
+                    </label>
+                    <Input
+                      className="form-control-alternative"
+                      id="manager-input-passwordr"
+                      placeholder="123123123"
+                      type="text"
+                      onChange={e => setPassword(e.target.value)}
+                    />
+                  </FormGroup>
+                </Col>
+                <Col md="12">
+                  <FormGroup>
+                    <label
+                      className="form-control-label"
+                      htmlFor="input-address"
+                    >
+                      Hospital Ward
+                    </label>
+                    <InputGroupButtonDropdown
+                      addonType="append"
+                      isOpen={dropdownOpen}
+                      toggle={toggleDropDown}
+                      className="form-control-alternative"
+                    >
+                      <DropdownToggle caret>
+                        {selectedHospital || "Choose Hospital"}
+                      </DropdownToggle>
+                      <DropdownMenu>
+                        {props.hospitals.map(hospital => {
+                          return (
+                            <div>
+                              <DropdownItem onClick={e => handleHospital(e)}>
+                                {hospital.name}
+                              </DropdownItem>
+                              <DropdownItem divider />
+                            </div>
+                          );
+                        })}
+                      </DropdownMenu>
+                    </InputGroupButtonDropdown>
+                  </FormGroup>
+                </Col>
+              </Row>
+            </div>
+            {hasError && <p>There is an error with adding the manager</p>}
+            {submitSuccess && <p>The form has been submitted</p>}
+            <Button
+              color="primary"
+              href="#pablo"
+              type="submit"
+              onClick={e => submitForm(e)}
+            >
+              Add manager
+            </Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </Col>
+  );
+};
 
 export default AddManagerForm;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,50 +1,58 @@
 import React from "react";
 
 // reactstrap components
-import { Card, CardBody, CardTitle, Container, Row, Col, Progress, Button } from "reactstrap";
-import AdminNavBar from "./AdminNavBar"
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  Container,
+  Row,
+  Col,
+  Progress,
+  Button
+} from "reactstrap";
+import AdminNavBar from "./AdminNavBar";
 
-const calculateBeds = (hospitals) => {
-    let totalBed = 0;
-    let availableBed = 0;
-    hospitals.map(hospital => {
-        totalBed += hospital.totalBeds
-        availableBed += hospital.availableBeds
-    });
-    return {totalBed, availableBed};
-}
+const calculateBeds = hospitals => {
+  let totalBed = 0;
+  let availableBed = 0;
+  hospitals.map(hospital => {
+    totalBed += hospital.totalBeds;
+    availableBed += hospital.availableBeds;
+  });
+  return { totalBed, availableBed };
+};
 
-const calculateStatus = ((availableBeds, totalBeds) => {
-    let status = 'unknown';
-    let dot = 'bg-success'
-    // const capacity = Math.round((1-(availableBeds / totalBeds))*100);
-    const capacity = calculateCapacity(availableBeds, totalBeds);
-    if (capacity ==0) {
-      status = 'No Capacity'
-      dot = 'bg-danger'
-    } else if (capacity >0 && capacity < 15) {
-      status = 'very limited capacity'
-      dot = 'bg-danger'
-    } else if (capacity >= 15 && capacity <30) {
-      status = 'limited capacity'
-      dot = 'bg-success'
-    } else {
-      status = 'good capacity'
-      dot = 'bg-info'
-    }
-  
-    return { status, dot };
-  })
-  
-  const calculateCapacity = ((availableBeds, totalBeds) => {
-    const capacity = Math.round((availableBeds/totalBeds)*100);
-    if(Number.isInteger(capacity)) {
-      return capacity
-    } else {
-      return 0
-    }
-  })
-  
+const calculateStatus = (availableBeds, totalBeds) => {
+  let status = "unknown";
+  let dot = "bg-success";
+  // const capacity = Math.round((1-(availableBeds / totalBeds))*100);
+  const capacity = calculateCapacity(availableBeds, totalBeds);
+  if (capacity == 0) {
+    status = "No Capacity";
+    dot = "bg-danger";
+  } else if (capacity > 0 && capacity < 15) {
+    status = "very limited capacity";
+    dot = "bg-danger";
+  } else if (capacity >= 15 && capacity < 30) {
+    status = "limited capacity";
+    dot = "bg-success";
+  } else {
+    status = "good capacity";
+    dot = "bg-info";
+  }
+
+  return { status, dot };
+};
+
+const calculateCapacity = (availableBeds, totalBeds) => {
+  const capacity = Math.round((availableBeds / totalBeds) * 100);
+  if (Number.isInteger(capacity)) {
+    return capacity;
+  } else {
+    return 0;
+  }
+};
 
 class Header extends React.Component {
   render() {
@@ -53,8 +61,8 @@ class Header extends React.Component {
     const capacity = calculateCapacity(availableBed, totalBed);
     return (
       <>
-        <div className="header bg-gradient-info pb-8 pt-5">
-        <AdminNavBar hospitals={this.props.hospitals}/>
+        <div className="header background pb-8 pt-5">
+          <AdminNavBar hospitals={this.props.hospitals} />
           <Container fluid>
             <div className="header-body">
               {/* Card stats */}
@@ -76,7 +84,7 @@ class Header extends React.Component {
                         </div>
                         <Col className="col-auto">
                           <div className="icon icon-shape bg-danger text-white rounded-circle shadow">
-                            <i className="fas fa-chart-bar" />
+                            <i className="fas fa-bed" />
                           </div>
                         </Col>
                       </Row>
@@ -106,7 +114,7 @@ class Header extends React.Component {
                         </div>
                         <Col className="col-auto">
                           <div className="icon icon-shape bg-warning text-white rounded-circle shadow">
-                            <i className="fas fa-chart-pie" />
+                            <i className="fas fa-bed" />
                           </div>
                         </Col>
                       </Row>
@@ -131,7 +139,9 @@ class Header extends React.Component {
                             Overall Status
                           </CardTitle>
                           <i className={totalStatus.dot} />
-                          <span className="h2 font-weight-bold mb-0">{totalStatus.status}</span>
+                          <span className="h2 font-weight-bold mb-0">
+                            {totalStatus.status}
+                          </span>
                         </div>
                         <Col className="col-auto">
                           <div className="icon icon-shape bg-yellow text-white rounded-circle shadow">
@@ -153,20 +163,20 @@ class Header extends React.Component {
                     <CardBody>
                       <Row>
                         <div className="col">
-                            <CardTitle
+                          <CardTitle
                             tag="h5"
                             className="text-uppercase text-muted mb-0"
-                            >
+                          >
                             Total Available Capacity
-                            </CardTitle>
-                            <span className="h2 font-weight-bold mb-0">
-                                {capacity}%
-                            </span>
-                            <Progress
-                                max="100"
-                                value={capacity}
-                                barClassName={totalStatus.dot}
-                            />
+                          </CardTitle>
+                          <span className="h2 font-weight-bold mb-0">
+                            {capacity}%
+                          </span>
+                          <Progress
+                            max="100"
+                            value={capacity}
+                            barClassName={totalStatus.dot}
+                          />
                         </div>
                         <Col className="col-auto">
                           <div className="icon icon-shape bg-info text-white rounded-circle shadow">
